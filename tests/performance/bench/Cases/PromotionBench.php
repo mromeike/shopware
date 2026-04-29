ffe567e578c3df9e26be217ad66287f848d1c896
Marcel Romeike <marcel.romeike@asambeauty.com>
2026-04-29 03:17:42 +0200

    Extract promotion short hands into trait

--- tests/performance/bench/Cases/PromotionBench.php
+++ tests/performance/bench/Cases/PromotionBench.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Shopware\Tests\Bench\Cases;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\ParameterType;
 use PhpBench\Attributes as Bench;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Checkout\Cart\LineItem\CartDataCollection;
@@ -15,6 +13,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SalesChannel\Context\SalesChannelContextService;
 use Shopware\Tests\Bench\AbstractBenchCase;
+use Shopware\Tests\Bench\Cases\Traits\PromotionBenchShortHands;
 use Shopware\Tests\Bench\Fixtures;
 
 /**
@@ -29,6 +28,8 @@ use Shopware\Tests\Bench\Fixtures;
  */
 class PromotionBench extends AbstractBenchCase
 {
+    use PromotionBenchShortHands;
+
     /**
      * The number of random customer entries to generate for `promotion.orders_per_customer_count`.
      */
@@ -155,57 +156,5 @@ class PromotionBench extends AbstractBenchCase
 
         // ~20 - ~26mb measured
         !\array_key_exists('PRINT_MEM_ALLOC', $_SERVER) ?: \printf("%02.6f mb\n", $allocMb);
-    }
-    /**
-     * Set up method for given promotion's totals.
-     *
-     * @param non-empty-array<string, positive-int> $promotionIds
-     * @throws \JsonException when the JSON serilaization fails
-     */
-    private function setUpTotals(array $promotionIds): void
-    {
-        if (empty($promotionIds)) {
-            throw new \LogicException('The promotion IDs must not be empty!');
-        }
-
-        foreach ($promotionIds as $promotionId => $iterations)
-        {
-            $totals = \iterator_to_array($this->generateTotals(iterations: $iterations, maxUsesPerCustomer: 10));
-
-            static::getContainer()->get(Connection::class)
-                ->executeStatement('UPDATE promotion SET order_count = :count, orders_per_customer_count = :customerCount WHERE id = :id', [
-                    'id' => Uuid::fromHexToBytes($this->ids->get($promotionId)),
-                    'count' => \array_sum($totals),
-                    'customerCount' => $json = \json_encode($totals, \JSON_THROW_ON_ERROR),
-                ], ['id' => ParameterType::BINARY]);
-
-            // The measured memory usage is not representative, as data is first generated as array before encoding as JSON,
-            //  which also takes up additional memory, so we ensure we are within the expected threshold of bytes.
-            // This JSON should be between about 2mb and 10mb in size.
-            $jsonSizeInMb = \strlen($json) / (1024 ** 2);
-            try {
-                TestCase::assertGreaterThan(2/*mb*/, $jsonSizeInMb);
-                TestCase::assertLessThan(10/*mb*/, $jsonSizeInMb);
-            } catch (\Throwable $e) {
-                \array_key_exists('PRINT_MEM_ALLOC_ERROR', $_SERVER) ? \printf("%s\n", $e->getMessage()) : throw $e;
-            }
-        }
-    }
-
-    /**
-     * Generate totals for the `promotion.orders_per_customer_count` column.
-     *
-     * @see \Shopware\Core\Checkout\Promotion\DataAbstractionLayer\PromotionRedemptionUpdater::update()
-     *
-     * @param positive-int $iterations
-     * @param positive-int $minUsesPerCustomer
-     * @param positive-int $maxUsesPerCustomer
-     * @return \Generator<string, positive-int>
-     */
-    private function generateTotals(int $iterations = 1, int $minUsesPerCustomer = 1, int $maxUsesPerCustomer = 2): \Generator
-    {
-        while (0 < $iterations--) {
-            yield Uuid::randomHex() => \rand($minUsesPerCustomer, $maxUsesPerCustomer);
-        }
     }
 }
---
+++ tests/performance/bench/Cases/Traits/PromotionBenchShortHands.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopware\Tests\Bench\Cases\Traits;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\Test\Stub\Framework\IdsCollection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Helper methods to provision large `promotion.orders_per_customer_count` payload for promotions.
+ *
+ * Available env-var parameters:
+ *
+ * - `PRINT_MEM_ALLOC_ERROR`: Prints the error caused by totals' JSON not meeting expected size (`1-10mb`).
+ *
+ * @property IdsCollection $ids
+ * 
+ * @internal - only for performance benchmarks
+ */
+trait PromotionBenchShortHands
+{
+    /**
+     * Set up method for given promotion's totals.
+     *
+     * @param non-empty-array<string, positive-int> $promotionIds
+     * @throws \JsonException when the JSON serilaization fails
+     */
+    private function setUpTotals(array $promotionIds): void
+    {
+        if (empty($promotionIds)) {
+            throw new \LogicException('The promotion IDs must not be empty!');
+        }
+
+        foreach ($promotionIds as $promotionId => $iterations)
+        {
+            $totals = \iterator_to_array($this->generateTotals(iterations: $iterations, maxUsesPerCustomer: 10));
+
+            static::getContainer()->get(Connection::class)
+                ->executeStatement('UPDATE promotion SET order_count = :count, orders_per_customer_count = :customerCount WHERE id = :id', [
+                    'id' => Uuid::fromHexToBytes($this->ids->get($promotionId)),
+                    'count' => \array_sum($totals),
+                    'customerCount' => $json = \json_encode($totals, \JSON_THROW_ON_ERROR),
+                ], ['id' => ParameterType::BINARY]);
+
+            // The measured memory usage is not representative, as data is first generated as array before encoding as JSON,
+            //  which also takes up additional memory, so we ensure we are within the expected threshold of bytes.
+            // This JSON should be between about 2mb and 10mb in size.
+            $jsonSizeInMb = \strlen($json) / (1024 ** 2);
+            try {
+                TestCase::assertGreaterThan(2/*mb*/, $jsonSizeInMb);
+                TestCase::assertLessThan(10/*mb*/, $jsonSizeInMb);
+            } catch (\Throwable $e) {
+                \array_key_exists('PRINT_MEM_ALLOC_ERROR', $_SERVER) ? \printf("%s\n", $e->getMessage()) : throw $e;
+            }
+
+            #\file_put_contents(__DIR__.'/../../../../integration/Core/Promotion/Cart/_fixtures/promotion__orders_per_customer_count.json', $json);
+        }
+    }
+
+    /**
+     * Generate totals for the `promotion.orders_per_customer_count` column.
+     *
+     * @see \Shopware\Core\Checkout\Promotion\DataAbstractionLayer\PromotionRedemptionUpdater::update()
+     *
+     * @param positive-int $iterations
+     * @param positive-int $minUsesPerCustomer
+     * @param positive-int $maxUsesPerCustomer
+     * @return \Generator<string, positive-int>
+     */
+    private function generateTotals(int $iterations = 1, int $minUsesPerCustomer = 1, int $maxUsesPerCustomer = 2): \Generator
+    {
+        while (0 < $iterations--) {
+            yield Uuid::randomHex() => \rand($minUsesPerCustomer, $maxUsesPerCustomer);
+        }
+    }
+
+    abstract protected static function getContainer(): ContainerInterface;
+}
